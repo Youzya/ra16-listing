aa054db5fa4cf0f9eb4783737deafc973c12d4e6
Youzya <holodilnikred@gmail.com>
2024-07-08 15:52:33 +0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './Main.css';
+import etsy from './data/etsy';
+import Listing from './components/Listing';
+
+function App() {
+  const processedItems = etsy.map(item => ({
+      listing_id: item.listing_id || "N/A",
+      url: item.url || "#",
+      MainImage: item.MainImage ? item.MainImage.url_570xN : 'https://prikolist.club/wp-content/uploads/2019/11/Screenshot_2-12.jpg',
+      title: item.title || "No description",
+      currency_code: item.currency_code || "-",
+      price: item.price || "-",
+      quantity: item.quantity || 0,
+    }));
+    return (
+      <Listing items={processedItems}></Listing>
+    );
+
+}
+
+export default App;
